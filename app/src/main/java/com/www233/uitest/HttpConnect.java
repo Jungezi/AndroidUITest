2a9305d2ba1680cdccd08b42a4df0495ebc404fd
Jungezi <wwwjungezi@gmail.com>
2024-07-05 16:40:21 +0800

    在HttpConnect类中处理网络请求

--- app/src/main/java/com/www233/uitest/HttpConnect.java
+++ app/src/main/java/com/www233/uitest/HttpConnect.java
@@ -1,5 +1,8 @@
 package com.www233.uitest;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 
@@ -22,15 +25,19 @@ import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class HttpConnect extends OkHttpClient {
     OkHttpClient httpClient;
     Map<String, List<Cookie>> cookies = new HashMap<>();
-    public HttpConnect() {
+    private Handler mHandler ;
+
+    public HttpConnect(Handler mHandler) {
+        this.mHandler = mHandler;
 
         httpClient = new OkHttpClient.Builder()
-                .cache(new Cache(new File("W:\\code\\test"), 1024*1024))
+                .cache(new Cache(new File("W:\\code\\test.txt"), 1024*1024))
                 .addInterceptor(new Interceptor() {
                     @NonNull
                     @Override
@@ -56,6 +63,120 @@ public class HttpConnect extends OkHttpClient {
                             return new ArrayList<>();
                     }
                 }).build();
+    }
+
+
+    public void getSync(String url) {
+        new Thread() {
+            @Override
+            public void run() {
+                Request build = new Request.Builder().url(url).build();
+                Call call = httpClient.newCall(build);
+                try {
+                    Response execute = call.execute();
+                    if (execute.body() != null) {
+                        String str = execute.body().string();
+                        System.out.println("getSync: " + str);
+
+                        Message message = new Message();
+                        message.what = 0;
+                        message.obj = "getSync: " + str;
+
+                        mHandler.sendMessage(message);
+
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }.start();
+
+    }
+
+
+    public void getAsync(String url) {
+        new Thread() {
+            @Override
+            public void run() {
+                Request build = new Request.Builder().url(url).build();
+                Call call = httpClient.newCall(build);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            String str = null;
+                            if (response.body() != null) {
+                                str = response.body().string();
+                                System.out.println("getAsync: " + str);
+
+                                Message message = new Message();
+                                message.what = 0;
+                                message.obj = "getAsync: " + str;
+
+                                mHandler.sendMessage(message);
+                            }
+                        }
+                    }
+                });
+            }
+        }.start();
+    }
+
+    public void postSync(String url, FormBody frombody) {
+        new Thread() {
+            @Override
+            public void run() {
+                Request request = new Request.Builder().url(url).post(frombody).build();
+                Call call = httpClient.newCall(request);
+                try {
+                    Response execute = call.execute();
+                    if (execute.body() != null) {
+                        String str = execute.body().string();
+
+                        Message message = new Message();
+                        message.what = 0;
+                        message.obj = "postSync: " + str;
+
+                        mHandler.sendMessage(message);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }.start();
+    }
+
+
+    public void postAsync(String url, RequestBody frombody) {
+        Request request = new Request.Builder().url(url).post(frombody).build();
+        Call call = httpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        String str = response.body().string();
+
+                        Message message = new Message();
+                        message.what = 0;
+                        message.obj = "postAsync: " + str;
+
+                        mHandler.sendMessage(message);
+                    }
+                }
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/www233/uitest/PracticeInternetActivity.java
+++ app/src/main/java/com/www233/uitest/PracticeInternetActivity.java
@@ -2,6 +2,9 @@ package com.www233.uitest;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -33,7 +36,7 @@ import okhttp3.Response;
 
 public class PracticeInternetActivity extends AppCompatActivity {
     private static final String TAG = "Practice";
-    OkHttpClient okHttpClient;
+    HttpConnect okHttpClient;
     TextView tv;
 
     @Override
@@ -58,187 +61,58 @@ public class PracticeInternetActivity extends AppCompatActivity {
 
     private void httpInit() {
 
-        okHttpClient = new HttpConnect().httpClient;
+        okHttpClient = new HttpConnect(new Handler(Looper.myLooper()){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+                String str = msg.obj.toString();
+                tv.setText(str);
+            }
+        });
     }
 
     public void getSync(View view) {
-        new Thread() {
-            @Override
-            public void run() {
-                Request build = new Request.Builder().url("https://httpbin.org/get?a=1&b=2").build();
-                Call call = okHttpClient.newCall(build);
-                try {
-                    Response execute = call.execute();
-                    if (execute.body() != null) {
-                        Log.e(TAG, "getSync: " + execute.body().string());
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }.start();
-
+        okHttpClient.getSync("https://httpbin.org/get?a=1&b=2");
     }
 
     public void getAsync(View view) {
-        new Thread() {
-            @Override
-            public void run() {
-                Request build = new Request.Builder().url("https://httpbin.org/get?a=1&b=2").build();
-                Call call = okHttpClient.newCall(build);
-                call.enqueue(new Callback() {
-                    @Override
-                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
-
-                    }
-
-                    @Override
-                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                        if (response.isSuccessful()) {
-                            Log.e(TAG, "onResponse: Success\n" + response.body().string());
-                        } else {
-                            Log.e(TAG, "onResponse: failed");
-                        }
-                    }
-                });
-            }
-        }.start();
+        okHttpClient.getAsync("https://httpbin.org/get?a=1&b=2");
     }
 
     public void postSync(View view) {
-        new Thread() {
-            @Override
-            public void run() {
-                FormBody frombody = new FormBody.Builder().add("a", "2").add("b", "yes").build();
-                Request request = new Request.Builder().url("https://httpbin.org/post").post(frombody).build();
-                Call call = okHttpClient.newCall(request);
-                try {
-                    Response execute = call.execute();
-                    if (execute.body() != null) {
-                        String string = execute.body().string();
-                        Log.e(TAG, "postSync run: " + string);
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }.start();
+        FormBody frombody = new FormBody.Builder().add("a", "2").add("b", "yes").build();
+        okHttpClient.postSync("https://httpbin.org/post", frombody);
     }
 
     public void postAsync(View view) {
         FormBody frombody = new FormBody.Builder().add("a", "2").add("b", "yes").build();
-        Request request = new Request.Builder().url("https://httpbin.org/post").post(frombody).build();
-        Call call = okHttpClient.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    if (response.body() != null) {
-                        Log.e(TAG, "postAsync onResponse: " + response.body().string());
-                    }
-                }
-            }
-        });
+        okHttpClient.postAsync("https://httpbin.org/post", frombody);
 
     }
 
     public void uploadFile(View view) {
-        new Thread() {
-            @Override
-            public void run() {
-                File file = new File("W:\\code\\test.txt");
-                Log.e(TAG, "uploadFile: new file");
-                RequestBody requestBody = RequestBody.create(file, MediaType.parse("text/plain"));
-
-                JSONObject json = new JSONObject();
-                try {
-                    json.put("a", "111");
-                    json.put("b", "wewewewew");
-                    json.put("cdcd", "dsadwqsfas");
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
-                Log.e(TAG, "run: " + json.toString());
-                RequestBody requestBody1 = RequestBody.create(json.toString().getBytes());
-
-                Log.e(TAG, "uploadFile: new RequestBody");
-//                MultipartBody multipartBody = new MultipartBody.Builder().addFormDataPart("file", file.getName(), requestBody).build();
-                Log.e(TAG, "uploadFile: new MultipartBody");
-                Request build = new Request.Builder().url("https://httpbin.org/post").post(requestBody1).build();
-                Call call = okHttpClient.newCall(build);
-                try {
-                    Log.e(TAG, "uploadFile: start execute");
-                    Response execute = call.execute();
-                    Log.e(TAG, "uploadFile: execute done.");
-                    if (execute.body() != null) {
-                        Log.e(TAG, "uploadFile: " + execute.body().string());
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }.start();
-
+        JSONObject json = new JSONObject();
+        try {
+            json.put("a", "111");
+            json.put("b", "wewewewew");
+            json.put("cdcd", "dsadwqsfas");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        Log.e(TAG, "run: " + json.toString());
+        RequestBody requestBody1 = RequestBody.create(json.toString().getBytes());
+        okHttpClient.postAsync("https://httpbin.org/post", requestBody1);
 
     }
 
     public void loginTest(View view) {
-
-        new Thread() {
-            @Override
-            public void run() {
-                FormBody formBody = new FormBody.Builder().add("username", "Jungezi")
-                        .add("password", "123w654001").build();
-                Request build = new Request.Builder().url("https://www.wanandroid.com/user/login").post(formBody).build();
-                Call call = okHttpClient.newCall(build);
-
-                Response response = null;
-                try {
-                    response = call.execute();
-                    if (response.isSuccessful()) {
-                        if (response.body() != null) {
-                            String str = response.body().string();
-                            System.out.println(str);
-                            runOnUiThread(() -> tv.setText(str));
-                        }
-
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }.start();
-
+        FormBody formBody = new FormBody.Builder().add("username", "Jungezi")
+                .add("password", "123w654001").build();
+        okHttpClient.postAsync("https://www.wanandroid.com/user/login", formBody);
 
     }
 
     public void accountList(View view) {
-        new Thread() {
-            @Override
-            public void run() {
-                Request build = new Request.Builder().url("https://www.wanandroid.com/lg/collect/list/0/json").build();
-                Call call = okHttpClient.newCall(build);
-
-                Response response = null;
-                try {
-                    response = call.execute();
-                    if (response.isSuccessful()) {
-                        if (response.body() != null) {
-                            String str = response.body().string();
-                            System.out.println(str);
-                            runOnUiThread(() -> tv.setText(str));
-                        }
-
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }.start();
+        okHttpClient.getAsync("https://www.wanandroid.com/lg/collect/list/0/json");
     }
 }
